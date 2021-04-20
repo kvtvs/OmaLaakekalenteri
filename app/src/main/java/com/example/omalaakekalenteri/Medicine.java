2c6f7059de4b441d6c6074cf4385881c45eda8b9
sossut <mikki.alakari@elisanet.fi>
2021-04-20 14:27:19 +0300

    Add Medicine class

---
+++ app/src/main/java/com/example/omalaakekalenteri/Medicine.java
@@ -0,0 +1,42 @@
+package com.example.omalaakekalenteri;
+
+public class Medicine {
+    private String name;
+    private int dosageMg;
+    private String activeIngredient;
+    private int timesADay;
+    private int quantity;
+
+    public Medicine(String name, int dosageMg, String activeIngredient, int timesADay, int quantity) {
+        this.name = name;
+        this.dosageMg = dosageMg;
+        this.activeIngredient = activeIngredient;
+        this.timesADay = timesADay;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDosageMg() {
+        return dosageMg;
+    }
+
+    public String getActiveIngredient() {
+        return activeIngredient;
+    }
+
+    public int getTimesADay() {
+        return timesADay;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
