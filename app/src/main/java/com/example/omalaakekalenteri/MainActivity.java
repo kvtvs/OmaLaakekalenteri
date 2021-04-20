4def24f407a189795f5e4608230d454134a1fc2b
sossut <mikki.alakari@elisanet.fi>
2021-04-20 15:34:29 +0300

    kämänen ListView ja Lisää lääke-button

--- app/src/main/java/com/example/omalaakekalenteri/MainActivity.java
+++ app/src/main/java/com/example/omalaakekalenteri/MainActivity.java
@@ -3,14 +3,36 @@ package com.example.omalaakekalenteri;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private ArrayList<Medicine> medicines;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        medicines = new ArrayList<>();
+        medicines.add(new Medicine("Burana", 400, "ibuprofeiini", 3, 21));
+        medicines.add(new Medicine("Panadol", 1000, "parasetamoli", 2, 16));
 
+        ListView listViewMedicines = findViewById(R.id.listViewMedicines);
+        listViewMedicines.setAdapter(new ArrayAdapter<Medicine>(
+                this, R.layout.medicine_item_layout, medicines
+        ));
+
+        listViewMedicines.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
+
+            }
+        });
     }
 }
