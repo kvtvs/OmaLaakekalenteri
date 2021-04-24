ff4250fd07c2bacda5b7e7413e0e07697096f97f
sossut <mikki.alakari@elisanet.fi>
2021-04-24 13:51:49 +0300

    Poista lääke listalta.

--- app/src/main/java/com/example/omalaakekalenteri/DisplayMedicine.java
+++ app/src/main/java/com/example/omalaakekalenteri/DisplayMedicine.java
@@ -3,17 +3,20 @@ package com.example.omalaakekalenteri;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.time.Instant;
 
 public class DisplayMedicine extends AppCompatActivity {
     private final String TAG = "MED_";
+    private int medicineNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +29,7 @@ public class DisplayMedicine extends AppCompatActivity {
         String activeIngredient = bundle.getString("activeIngredient");
         String timesADay = bundle.getString("timesADay");
         String quantity = bundle.getString("quantity");
-        int medicineNumber = bundle.getInt("medicineNumber");
+        medicineNumber = bundle.getInt("medicineNumber");
 
 
         TextView textViewName = findViewById(R.id.textViewMedicineName);
@@ -50,5 +53,7 @@ public class DisplayMedicine extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
     }
+
 }
