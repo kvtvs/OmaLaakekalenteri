2123b438bbf9f179c3ffb624da1e841077586e12
sossut <mikki.alakari@elisanet.fi>
2021-04-26 13:04:14 +0300

    DisplayMedicine fixi.

--- app/src/main/java/com/example/omalaakekalenteri/AddMedicine.java
+++ app/src/main/java/com/example/omalaakekalenteri/AddMedicine.java
@@ -5,13 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
-import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class AddMedicine extends AppCompatActivity  {
 
