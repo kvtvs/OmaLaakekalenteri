11fab6613a6755ce3bdb4c5133af71dfdd10b4c2
kvtvs <kata.sara-aho@metropolia.fi>
2021-04-27 11:22:06 +0300

    Some changes

--- app/src/main/java/com/example/omalaakekalenteri/DisplayMedicine.java
+++ app/src/main/java/com/example/omalaakekalenteri/DisplayMedicine.java
@@ -1,28 +1,24 @@
 package com.example.omalaakekalenteri;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Dialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.gson.Gson;
-
-import java.time.Instant;
 
 public class DisplayMedicine extends AppCompatActivity implements RemoveMedicineDialog.RemoveMedicineDialogListener{
     private final String TAG = "MED_";
     private int medicineNumber;
     public final static String SHARED_PREFS = "sharedPrefs";
     public final static String LIST = "list";
+    private TextView warningText;
+    private TextView textViewQuantity;
+    private String quantity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,15 +30,16 @@ public class DisplayMedicine extends AppCompatActivity implements RemoveMedicine
         String dosage = bundle.getString("dosage");
         String activeIngredient = bundle.getString("activeIngredient");
         String timesADay = bundle.getString("timesADay");
-        String quantity = bundle.getString("quantity");
+        quantity = bundle.getString("quantity");
         medicineNumber = bundle.getInt("medicineNumber");
 
-
+        warningText = (TextView) findViewById(R.id.textViewWarning);
+        warningText.setEnabled(false);
         TextView textViewName = findViewById(R.id.textViewMedicineName);
         TextView textViewDosage = findViewById(R.id.textViewMedicineDosage);
         TextView textViewActiveIngredient = findViewById(R.id.textViewMedicineActiveIngredient);
         TextView textViewTimesADay = findViewById(R.id.textViewMedicineTimesADay);
-        TextView textViewQuantity = findViewById(R.id.textViewMedicineQuantity);
+        textViewQuantity = findViewById(R.id.textViewMedicineQuantity);
         Button buttonRemoveMedicine = findViewById(R.id.buttonRemoveMedicine);
         Button buttonReturnToList = findViewById(R.id.buttonReturnToList);
 
@@ -68,6 +65,8 @@ public class DisplayMedicine extends AppCompatActivity implements RemoveMedicine
             }
         });
     }
+
+
     public void openDialog(){
         RemoveMedicineDialog dialog = new RemoveMedicineDialog();
         dialog.show(getSupportFragmentManager(), "remove medicine dialog");
@@ -87,4 +86,6 @@ public class DisplayMedicine extends AppCompatActivity implements RemoveMedicine
         Intent intent = new Intent(DisplayMedicine.this, DisplayMedicineList.class);
         startActivity(intent);
     }
+
+
 }
