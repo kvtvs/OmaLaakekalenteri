97a32ce6201c4a2d8de2d3c226fc76adbd517b88
sossut <mikki.alakari@elisanet.fi>
2021-04-29 14:01:36 +0300

    kommentteja ja järkkäystä

--- app/src/main/java/com/example/omalaakekalenteri/MainActivity.java
+++ app/src/main/java/com/example/omalaakekalenteri/MainActivity.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -17,6 +18,7 @@ import androidx.fragment.app.DialogFragment;
 import java.util.ArrayList;
 
 /**
+ *
  * @author Kata Sara-aho, Mikko Räikkönen, Mikael Alakari
  */
 public class MainActivity extends AppCompatActivity /*implements Serializable*/ implements TimePickerDialog.OnTimeSetListener {
@@ -63,11 +65,13 @@ public class MainActivity extends AppCompatActivity /*implements Serializable*/ 
     }
 
 
-    /** Receives result info from AddMedicine.class that a new medicine is added **/
+    /**
+     *  Receives result info from AddMedicine.class that a new medicine is added
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent intent){
         super.onActivityResult(requestCode, resultCode, intent);
-
+        Log.d("MED_", "onActivityResult() called");
         if(requestCode == 1 && resultCode == RESULT_OK){
             /** Creates new medicine and adds it to ArratList Adapter **/
             Medicine med = new Medicine(intent.getStringExtra("laakeNimi"), intent.getStringExtra("vaikuttavaAine"), intent.getIntExtra("kertaaPaivassa", 0), intent.getIntExtra("maara", 0), intent.getIntExtra("annostus", 0),intent.getIntExtra("kappaleMaara", 0));
@@ -77,7 +81,9 @@ public class MainActivity extends AppCompatActivity /*implements Serializable*/ 
         }
     }
 
-    /** Receives result info from AddMedicine.class that a new medicine is added **/
+    /**
+     *  Receives result info from AddMedicine.class that a new medicine is added
+     */
     public void updateListView(){
         /** Fetches the info of the new medicine from Intent **/
         Bundle bundle = getIntent().getExtras();
@@ -90,14 +96,18 @@ public class MainActivity extends AppCompatActivity /*implements Serializable*/ 
         }
     }
 
-    /** Method for opening the calendar activity **/
+    /**
+     Method for opening the calendar activity
+     */
     public void openActivityCalendar30(){
         Intent intent = new Intent(this, calendar30.class);
         startActivity(intent);
 
     }
 
-    /** Method for opening the medicine list activity **/
+    /**
+     * Method for opening the medicine list activity
+     */
     public void openMedicineList() {
         Intent intent = new Intent(this, DisplayMedicineList.class);
         startActivity(intent);
